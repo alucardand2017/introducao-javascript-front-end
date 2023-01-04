4127582f97dcb6c3000b50667da07c857b10ebda
Anderson do Nascimento <anderson_ns5@hotmail.com>
2023-01-04 12:04:16 -0300

    adicionando alguns exercicios

---
+++ JS exercicios/tipos.js
@@ -0,0 +1,29 @@
+const numeroInteiro = 1;
+const outroInteiro = 3;
+const numeroFloat = 1.0;
+const outroFloat = 3.0;
+const texto1 = "Meu nome é ";
+const texto2 = "Anderson";
+const resultadoInteiro = numeroInteiro/outroInteiro;
+const resultadoFloat = numeroFloat/outroFloat;
+const resultadoString = texto1+texto2;
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+const inputMinusculo = input.toLowerCase();
+const n1 = 5;
+const n2 = 10;
+
+console.log(n1===n2);
+console.log(cidade === inputMinusculo); // true
+console.log(cidade === input); // false
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+console.log(resultadoInteiro);
+console.log(resultadoFloat);
+console.log(resultadoString);
